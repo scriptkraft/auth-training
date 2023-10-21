1f4795fe979dda822c1252487060618710f71119
Swanand <swanand.nirgudkar@gmail.com>
2023-10-22 02:08:41 +0530

    master: Base functionality

---
+++ AuthTraining/Controllers/BasicController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace AuthTraining.Controllers
+{
+    [RoutePrefix("basic")]
+    public class BasicController : ApiController
+    {
+        [Route("something")]
+        [HttpGet]
+        public async Task<IHttpActionResult> DoSomething()
+        {
+            return Ok("this function does something");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # auth-training
 Web Application Authentication Training
+
+Visual Studio
+> Create a new project
+> ASP.NET Web Application (.NET Framework)
+> Empty template using Web API
+
+For adding new controller
+> Add Controller
+> Web API 2 Controller - Empty
+
+Links:
+https://learn.microsoft.com/en-us/aspnet/web-api/overview/web-api-routing-and-actions/attribute-routing-in-web-api-2
